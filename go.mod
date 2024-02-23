cd331023908c8a52c160c17d4d0445a0f4a0e3d6
Sage Hourihan <samiho97@gmail.com>
2024-02-23 15:49:10 -0500

    added validation using edi-nation api

--- go.mod
+++ go.mod
@@ -1,5 +1,3 @@
 module github.com/SageHourihan/JEDI
 
 go 1.21.6
-
-require github.com/pbjer/hedi v0.0.0-20230916031652-001d4a47b8e0
--- helper.go
+++ helper.go
@@ -2,25 +2,44 @@ package main
 
 import (
 	"fmt"
+	"io"
+	"net/http"
 	"os"
 	"strings"
-	"github.com/pbjer/hedi"
 )
+
+const url string = "https://api.edination.com/v2/x12/read?ignoreNullValues=false&continueOnError=false&charSet=utf-8"
+const method string = "POST"
 
 func validate(file string) {
 
 	f, _ := os.ReadFile(file)
 
-	blah :=  string(f)
+	payload := strings.NewReader(string(f))
 
-	reader := strings.NewReader(blah)
-	fmt.Println(reader)
-	lexer := hedi.NewLexer(reader)
-	tokens, err := lexer.Tokens()
+	client := &http.Client{}
+	req, err := http.NewRequest(method, url, payload)
 
 	if err != nil {
-		fmt.Println(tokens)
+		fmt.Println(err)
+		return
 	}
+	req.Header.Add("Ocp-Apim-Subscription-Key", "3ecf6b1c5cf34bd797a5f4c57951a1cf")
+	req.Header.Add("Content-Type", "application/octet-stream")
+
+	res, err := client.Do(req)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	defer res.Body.Close()
+
+	body, err := io.ReadAll(res.Body)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	fmt.Println(string(body))
 }
 
 func translate(file string) {
