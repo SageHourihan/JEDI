fa5ca553af935e3ab336cc03601bf4a291ed20eb
Sage Hourihan <samiho97@gmail.com>
2024-02-23 15:26:11 -0500

    working on validation

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/SageHourihan/JEDI
 
 go 1.21.6
+
+require github.com/pbjer/hedi v0.0.0-20230916031652-001d4a47b8e0
--- helper.go
+++ helper.go
@@ -2,11 +2,25 @@ package main
 
 import (
 	"fmt"
+	"os"
+	"strings"
+	"github.com/pbjer/hedi"
 )
 
 func validate(file string) {
-	fmt.Println(file)
 
+	f, _ := os.ReadFile(file)
+
+	blah :=  string(f)
+
+	reader := strings.NewReader(blah)
+	fmt.Println(reader)
+	lexer := hedi.NewLexer(reader)
+	tokens, err := lexer.Tokens()
+
+	if err != nil {
+		fmt.Println(tokens)
+	}
 }
 
 func translate(file string) {
