8d2c2ae63a27f026fc4a83a778112a1088dd7cf7
Sage Hourihan <samiho97@gmail.com>
2024-02-23 13:56:03 -0500

    added validate and translate functions. added test

---
+++ helper.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"fmt"
+)
+
+func validate(file string) {
+	fmt.Println(file)
+
+}
+
+func translate(file string) {
+	fmt.Println("Translate logic")
+}
---
+++ main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"flag"
+)
+
+func main() {
+
+	v := flag.String("v", "Please provide file path", "Used to validate an EDI file")
+	t := flag.String("t", "Please provide file path", "Used to translate an EDI file")
+	flag.Parse()
+
+	flag.Visit(func(f *flag.Flag) {
+		if f.Name == "v" {
+			validate(*v)
+		} else if f.Name == "t" {
+			translate(*t)
+		}
+	})
+}
+
+
